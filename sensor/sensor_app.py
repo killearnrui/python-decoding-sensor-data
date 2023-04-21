c5e3417b77b290dc2e174fc3186217ebbde38f4b
killearnrui <kangrui2002@yahoo.com>
2023-04-21 10:10:21 -0400

    checking

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -9,11 +9,10 @@ from load_data import load_sensor_data
 data = []
 print("Sensor Data App")
 ##############################
-data = load_sensor_data()
-print("Loaded records: {}".format(len(data)))
 
 # Module 1 code here:
-
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
