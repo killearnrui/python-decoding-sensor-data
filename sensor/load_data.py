453a8398c8706a187a925b3199f199be86d99421
killearnrui <kangrui2002@yahoo.com>
2023-04-21 10:05:34 -0400

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -6,7 +6,7 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in data_file:
+            for row in sensor_data:
                 sensor_data.append(row)
                 
     return sensor_data
