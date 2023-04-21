8de870b88331c4beec1d4f430464f00548976e85
killearnrui <kangrui2002@yahoo.com>
2023-04-21 10:59:01 -0400

    checking

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -11,9 +11,9 @@ class TemperatureData(HouseInfo):
     
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("temperature", rec_area)
-        return _convert_data(recs)
+        return self._convert_data(recs)
     
     def get_data_by_date(self, rec_date=date.today()):
-        recs = super().get_data_by_datea("temperature", rec_date)
-        return _convert_data(recs)
+        recs = super().get_data_by_date("temperature", rec_date)
+        return self._convert_data(recs)
       
