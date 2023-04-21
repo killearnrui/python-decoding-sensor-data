b82869cd9543d5d213d487be8862270af419753b
killearnrui <kangrui2002@yahoo.com>
2023-04-21 10:34:58 -0400

    checking

---
+++ sensor/house_info.py
@@ -0,0 +1,25 @@
+from datetime import datetime, date
+
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area = 0):
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record['area'])
+        return field_data
+    
+    def get_data_by_date(self, field, rec_date):
+        field_data = []
+        for record in self.data:
+            if record[date] == strftime(rec_date):
+                field_data.append(record[field])
+
+
+
+
+            
